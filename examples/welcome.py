1926a3c5ba00ed70f4fc7b108a3a3fe683b3b8c1
Richard Hull <rm_hull@yahoo.co.uk>
2017-02-26 00:07:46 +0000

    Add color

--- examples/welcome.py
+++ examples/welcome.py
@@ -83,6 +83,33 @@ welcome = [
     u"Bonvenon"
 ]
 
+colors = [
+    "lightpink", "pink", "crimson", "lavenderblush", "palevioletred", "hotpink",
+    "deeppink", "mediumvioletred", "orchid", "thistle", "plum", "violet",
+    "magenta", "fuchsia", "darkmagenta", "purple", "mediumorchid", "darkviolet",
+    "darkorchid", "indigo", "blueviolet", "mediumpurple", "mediumslateblue",
+    "slateblue", "darkslateblue", "lavender", "ghostwhite", "blue", "mediumblue",
+    "midnightblue", "darkblue", "navy", "royalblue", "cornflowerblue",
+    "lightsteelblue", "lightslategray", "slategray", "dodgerblue", "aliceblue",
+    "steelblue", "lightskyblue", "skyblue", "deepskyblue", "lightblue",
+    "powderblue", "cadetblue", "azure", "lightcyan", "paleturquoise", "cyan",
+    "aqua", "darkturquoise", "darkslategray", "darkcyan", "teal",
+    "mediumturquoise", "lightseagreen", "turquoise", "aquamarine",
+    "mediumaquamarine", "mediumspringgreen", "mintcream", "springgreen",
+    "mediumseagreen", "seagreen", "honeydew", "lightgreen", "palegreen",
+    "darkseagreen", "limegreen", "lime", "forestgreen", "green", "darkgreen",
+    "chartreuse", "lawngreen", "greenyellow", "darkolivegreen", "yellowgreen",
+    "olivedrab", "beige", "lightgoldenrodyellow", "ivory", "lightyellow",
+    "yellow", "olive", "darkkhaki", "lemonchiffon", "palegoldenrod", "khaki",
+    "gold", "cornsilk", "goldenrod", "darkgoldenrod", "floralwhite", "oldlace",
+    "wheat", "moccasin", "orange", "papayawhip", "blanchedalmond", "navajowhite",
+    "antiquewhite", "tan", "burlywood", "bisque", "darkorange", "linen", "peru",
+    "peachpuff", "sandybrown", "chocolate", "saddlebrown", "seashell", "sienna",
+    "lightsalmon", "coral", "orangered", "darksalmon", "tomato", "mistyrose",
+    "salmon", "snow", "lightcoral", "rosybrown", "indianred", "red", "brown",
+    "firebrick", "darkred", "maroon", "white", "whitesmoke", "gainsboro",
+    "lightgrey", "silver", "darkgray", "gray", "dimgray", "black"
+]
 
 def make_font(name, size):
     font_path = os.path.abspath(os.path.join(
@@ -165,9 +192,12 @@ def main():
     sq = device.width * 2
     virtual = viewport(device, sq, sq)
 
+    color_gen = pairs(infinite_shuffle(colors))
+
     for welcome_a, welcome_b in pairs(infinite_shuffle(welcome)):
-        widget_a = make_snapshot(device.width, device.height, welcome_a, fonts)
-        widget_b = make_snapshot(device.width, device.height, welcome_b, fonts)
+        color_a, color_b = color_gen.next()
+        widget_a = make_snapshot(device.width, device.height, welcome_a, fonts, color_a)
+        widget_b = make_snapshot(device.width, device.height, welcome_b, fonts, color_b)
 
         posn_a = random_point(virtual.width - device.width, virtual.height - device.height)
         posn_b = random_point(virtual.width - device.width, virtual.height - device.height)
