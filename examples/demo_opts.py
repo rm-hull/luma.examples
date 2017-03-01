d91c4adaaae3177adf2759231b2603cd0f61138b
Richard Hull <rm_hull@yahoo.co.uk>
2017-03-01 21:22:58 +0000

    ST7735 config settings

--- examples/demo_opts.py
+++ examples/demo_opts.py
@@ -26,7 +26,7 @@ parser = argparse.ArgumentParser(description='luma.examples arguments',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('--config', '-f', type=str, help='Load configuration settings from a file')
-parser.add_argument('--display', '-d', type=str, default='ssd1306', help='Display type, supports real devices or emulators', choices=["ssd1306", "ssd1322", "ssd1325", "ssd1331", "sh1106", "pcd8544", "max7219", "capture", "pygame", "gifanim"])
+parser.add_argument('--display', '-d', type=str, default='ssd1306', help='Display type, supports real devices or emulators', choices=["ssd1306", "ssd1322", "ssd1325", "ssd1331", "sh1106", "pcd8544", "st7735", "max7219", "capture", "pygame", "gifanim"])
 parser.add_argument('--width', type=int, default=128, help='Width of the device in pixels')
 parser.add_argument('--height', type=int, default=64, help='Height of the device in pixels')
 parser.add_argument('--rotate', '-r', type=int, default=0, help='Rotation factor', choices=[0, 1, 2, 3])
@@ -38,7 +38,8 @@ parser.add_argument('--spi-device', type=int, default=0, help='SPI device')
 parser.add_argument('--spi-bus-speed', type=int, default=8000000, help='SPI max bus speed (Hz)')
 parser.add_argument('--bcm-data-command', type=int, default=24, help='BCM pin for D/C RESET (SPI devices only)')
 parser.add_argument('--bcm-reset', type=int, default=25, help='BCM pin for RESET (SPI devices only)')
-parser.add_argument('--bcm-backlight', type=int, default=18, help='BCM pin for backlight (PCD8544 devices only)')
+parser.add_argument('--bcm-backlight', type=int, default=18, help='BCM pin for backlight (PCD8544, ST7735 devices only)')
+parser.add_argument('--framebuffer', type=str, default='diff_to_previous', help='Framebuffer implementation', choices=['diff_to_previous', 'full_frame'])
 parser.add_argument('--block-orientation', type=str, default='horizontal', help='Fix 90° phase error (MAX7219 LED matrix only)', choices=['horizontal', 'vertical'])
 parser.add_argument('--transform', type=str, default='scale2x', help='Scaling transform to apply (emulator only)', choices=["none", "identity", "scale2x", "smoothscale", "led_matrix", "seven_segment"])
 parser.add_argument('--scale', type=int, default=2, help='Scaling factor to apply (emulator only)')
@@ -84,11 +85,12 @@ if args.display in ('ssd1306', 'ssd1322', 'ssd1325', 'ssd1331', 'sh1106'):
                                           bcm_DC=args.bcm_data_command,
                                           bcm_RST=args.bcm_reset)
         device = Device(serial, width=args.width, height=args.height,
-                        rotate=args.rotate, mode=args.mode)
+                        rotate=args.rotate, mode=args.mode,
+                        framebuffer=args.framebuffer)
     except Exception as e:
         parser.error(e)
 
-elif args.display in ('pcd8544'):
+elif args.display in ('pcd8544', 'st7735'):
     import luma.lcd.device
     Device = getattr(luma.lcd.device, args.display)
     try:
@@ -98,7 +100,9 @@ elif args.display in ('pcd8544'):
                                       bcm_DC=args.bcm_data_command,
                                       bcm_RST=args.bcm_reset)
         luma.lcd.device.backlight(bcm_LIGHT=args.bcm_backlight).enable(True)
-        device = Device(serial, rotate=args.rotate)
+        device = Device(serial, width=args.width, height=args.height,
+                        rotate=args.rotate, mode=args.mode,
+                        framebuffer=args.framebuffer)
 
     except Exception as e:
         parser.error(e)
