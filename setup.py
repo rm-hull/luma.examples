4d137912676323dcd899ff8ea784d5f21f4f0616
Richard Hull <rm_hull@yahoo.co.uk>
2017-03-02 20:36:45 +0000

    Specify min versions

--- setup.py
+++ setup.py
@@ -27,10 +27,11 @@ setup(
     keywords="raspberry orange banana pi rpi opi sbc oled lcd led display screen spi i2c",
     url="https://github.com/rm-hull/luma.examples",
     install_requires=[
-        "luma.emulator",
-        "luma.oled",
-        "luma.lcd",
-        "luma.led_matrix"
+        "luma.core>=0.4.3",
+        "luma.emulator>=0.1.2",
+        "luma.oled>=2.2.5",
+        "luma.lcd>=0.2.2",
+        "luma.led_matrix>=0.6.1"
     ],
     setup_requires=pytest_runner,
     tests_require=test_deps,
