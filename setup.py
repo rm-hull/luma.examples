eb21fe06969276f6f7b4df3d3ecb15d175dd28a4
Richard Hull <rm_hull@yahoo.co.uk>
2017-05-01 22:51:46 +0100

    Bump dependencies

--- setup.py
+++ setup.py
@@ -46,11 +46,11 @@ setup(
     keywords="raspberry orange banana pi rpi opi sbc oled lcd led display screen spi i2c",
     url="https://github.com/rm-hull/luma.examples",
     install_requires=[
-        "luma.core>=0.9.0",
+        "luma.core>=0.9.1",
         "luma.emulator>=0.1.2",
         "luma.oled>=2.2.5",
         "luma.lcd>=0.3.4",
-        "luma.led_matrix>=0.7.0",
+        "luma.led_matrix>=0.10.1",
         "argcomplete"
     ],
     setup_requires=pytest_runner,
