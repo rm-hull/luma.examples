937eb9c7785ae8abb86edf653d8100a68a3463c4
Richard Hull <rm_hull@yahoo.co.uk>
2017-12-30 16:15:54 +0000

    Test for 'segment_mapper' attribute

--- examples/sevensegment_demo.py
+++ examples/sevensegment_demo.py
@@ -69,9 +69,8 @@ def show_message_alt(seg, msg, delay=0.1):
 def main():
     # create seven segment device
     device = get_device()
-    device_name = str(device.__class__.__name__)
 
-    if device_name not in ['ht1621', 'max7219']:
+    if not hasattr(device, 'segment_mapper'):
         sys.exit('sevensegment is not supported on a {} device'.format(
             device_name))
 
